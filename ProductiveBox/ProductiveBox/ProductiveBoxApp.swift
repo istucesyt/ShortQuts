8b2a4585062720db0400faac98a0af730f5c53b1
iStuces <108399865+istucesyt@users.noreply.github.com>
2023-11-25 18:06:08 +0100

    Add files via upload

---
+++ ProductiveBox/ProductiveBox/ProductiveBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductiveBoxApp.swift
+//  ProductiveBox
+//
+//  Created by Lucas Drouot on 25/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProductiveBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
